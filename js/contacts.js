ba559e5151f9b59eda42384fa05eba02375c3cb7
didathepro <didovicgraz@gmail.com>
2024-04-05 23:40:00 +0200

    edit window in progress

--- js/contacts.js
+++ js/contacts.js
@@ -188,7 +188,7 @@ function returnContactInfo(name,email,phone,color,firstLetter,secondLetter,conta
             <div class="name-container">
                 <h3>${name}</h3>
                 <div class="edit-delete">
-                    <div class="edit"><svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <div class="edit" onclick="editContact(${contact})"><svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M2 17H3.4L12.025 8.375L10.625 6.975L2 15.6V17ZM16.3 6.925L12.05 2.725L13.45 1.325C13.8333 0.941667 14.3042 0.75 14.8625 0.75C15.4208 0.75 15.8917 0.941667 16.275 1.325L17.675 2.725C18.0583 3.10833 18.2583 3.57083 18.275 4.1125C18.2917 4.65417 18.1083 5.11667 17.725 5.5L16.3 6.925ZM14.85 8.4L4.25 19H0V14.75L10.6 4.15L14.85 8.4Z" fill="#2A3647"/>
                     </svg>
                     <h5>Edit</h5></div>
@@ -208,6 +208,54 @@ function returnContactInfo(name,email,phone,color,firstLetter,secondLetter,conta
         </div>
     </div>
     `;
+}
+
+function editContact(contact) {
+    const [firstLetter, secondLetter] = findFirstLetters(contact);
+    console.log(firstLetter)
+    let content = document.getElementById('editContactContainer');
+    content.classList.remove('hidden');
+    content.innerHTML = "";
+    content.innerHTML += `
+    <div class="left-add">
+        <img src="../assets/img/Capa 1.png">
+        <h4>Edit contact</h4>
+    </div>
+    <div class="right-add">
+        <svg width="64" height="54" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M22.0001 22.0001C19.0667 22.0001 16.5556 20.9556 14.4667 18.8667C12.3779 16.7779 11.3334 14.2667 11.3334 11.3334C11.3334 8.40008 12.3779 5.88897 14.4667 3.80008C16.5556 1.71119 19.0667 0.666748 22.0001 0.666748C24.9334 0.666748 27.4445 1.71119 29.5334 3.80008C31.6223 5.88897 32.6667 8.40008 32.6667 11.3334C32.6667 14.2667 31.6223 16.7779 29.5334 18.8667C27.4445 20.9556 24.9334 22.0001 22.0001 22.0001ZM38.0001 43.3334H6.00008C4.53341 43.3334 3.27786 42.8112 2.23341 41.7668C1.18897 40.7223 0.666748 39.4668 0.666748 38.0001V35.8667C0.666748 34.3556 1.05564 32.9667 1.83341 31.7001C2.61119 30.4334 3.64453 29.4667 4.93341 28.8001C7.68897 27.4223 10.489 26.389 13.3334 25.7001C16.1779 25.0112 19.0667 24.6667 22.0001 24.6667C24.9334 24.6667 27.8223 25.0112 30.6667 25.7001C33.5112 26.389 36.3112 27.4223 39.0667 28.8001C40.3556 29.4667 41.389 30.4334 42.1667 31.7001C42.9445 32.9667 43.3334 34.3556 43.3334 35.8667V38.0001C43.3334 39.4668 42.8112 40.7223 41.7668 41.7668C40.7223 42.8112 39.4668 43.3334 38.0001 43.3334ZM6.00008 38.0001H38.0001V35.8667C38.0001 35.3779 37.8779 34.9334 37.6334 34.5334C37.389 34.1334 37.0667 33.8223 36.6667 33.6001C34.2668 32.4001 31.8445 31.5001 29.4001 30.9001C26.9556 30.3001 24.489 30.0001 22.0001 30.0001C19.5112 30.0001 17.0445 30.3001 14.6001 30.9001C12.1556 31.5001 9.73341 32.4001 7.33342 33.6001C6.93341 33.8223 6.61119 34.1334 6.36675 34.5334C6.1223 34.9334 6.00008 35.3779 6.00008 35.8667V38.0001ZM22.0001 16.6667C23.4667 16.6667 24.7223 16.1445 25.7668 15.1001C26.8112 14.0556 27.3334 12.8001 27.3334 11.3334C27.3334 9.86675 26.8112 8.61119 25.7668 7.56675C24.7223 6.5223 23.4667 6.00008 22.0001 6.00008C20.5334 6.00008 19.2779 6.5223 18.2334 7.56675C17.189 8.61119 16.6667 9.86675 16.6667 11.3334C16.6667 12.8001 17.189 14.0556 18.2334 15.1001C19.2779 16.1445 20.5334 16.6667 22.0001 16.6667Z" fill="white"/>
+        </svg>
+    
+        <div class="add-contact">
+            <section class="logInContainer">
+                <form id="addContactForm">
+                    <div class="email-container">
+                        <input id="name" class="input_field name" placeholder="Name">
+                        <img class="lock-icon" src="../assets/img/person.png" alt="">
+                    </div>
+                    <div class="email-container">
+                        <input id="email" class="input_field email" required type="email" placeholder="Email">
+                        <img class="mail-icon" src="../assets/img/mail.png">
+                    </div>
+                    <div class="email-container">
+                        <input id="phone" class="input_field email" required type="phone" placeholder="Phone">
+                        <img class="mail-icon" src="../assets/img/call.png">
+                    </div>
+
+                    <div class="buttons-container">
+                        <button class="cancel" onclick="closeContainer('editContactContainer');deleteContact(${contact})">Delete</button>
+                        <button type="button" class="create" onclick="addContactToArray();closeContainer('editContactContainer');">Save<img src="../assets/img/Vector (2).png"></button>
+                    </div>
+                </form>
+            </section>
+        </div>
+    </div>
+    `;
+    document.addEventListener('mouseup', function(e) {
+        if (!content.contains(e.target)) {
+        content.classList.add('hidden');
+        }
+    });
 }
 
 function addBgColor(index) {
@@ -260,11 +308,11 @@ function addNewContact() {
                     </div>
 
                     <div class="buttons-container">
-                        <button class="cancel" onclick="closeAddContactContainer();">Cancel <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <button class="cancel" onclick="closeContainer('addContactContainer');">Cancel <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M7.001 6.50008L12.244 11.7431M1.758 11.7431L7.001 6.50008L1.758 11.7431ZM12.244 1.25708L7 6.50008L12.244 1.25708ZM7 6.50008L1.758 1.25708L7 6.50008Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                         </button>
-                        <button type="button" class="create" onclick="addContactToArray();closeAddContactContainer();">Create contact<img src="../assets/img/Vector (2).png"></button>
+                        <button type="button" class="create" onclick="addContactToArray();closeContainer('addContactContainer');">Create contact<img src="../assets/img/Vector (2).png"></button>
                     </div>
                 </form>
             </section>
@@ -274,8 +322,8 @@ function addNewContact() {
 
 }
 
-function closeAddContactContainer() {
-    document.getElementById("addContactContainer").classList.add("hidden");
+function closeContainer(id) {
+    document.getElementById(id).classList.add("hidden");
 }
 
 function addContactToArray() {
