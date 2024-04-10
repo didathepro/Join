313d52a3720bc4845622f7c5565e2496944a153a
Matthias146 <126074233+Matthias146@users.noreply.github.com>
2024-04-10 18:09:37 +0200

    added storage file

--- js/login.js
+++ js/login.js
@@ -1,5 +1,29 @@
 const urlParams = new URLSearchParams(window.location.search);
 const msg = urlParams.get('msg');
+const STORAGE_TOKEN = 'K9SFYBX0R1WD1NS9ZKIOB4W1CL157WB4C579A62K';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+function init() {
+    document.getElementById('imgJoin');
+    document.body.classList.add('animation');
+    show();
+}
+
+function show() {
+    let box = document.getElementById('topBox');
+    let bodyAnimation = document.getElementById('bodyAnimation');
+    setTimeout(() => {
+        bodyAnimation.style.display = 'none'
+    }, 0)
+    setTimeout(() => {
+        box.style.display = 'flex';
+    }, 1000);
+    setTimeout(() => {
+        bodyAnimation.style.display = 'block'
+    }, 1500)
+};
+
+
 if (msg) {
     msgBox.innerHTML = msg;
 } else {
@@ -14,3 +38,4 @@ function login() {
         console.log('User Gefunden');
     }
 }
+
