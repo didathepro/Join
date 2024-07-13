f0225a58d6502781fd58c8e4f4a39676c16585eb
Andreas W. <bahamut13+github0@gmail.com>
2024-07-14 01:18:58 +0200

    storage.js JSDoc, max 14 lines and cleanup

--- js/storage.js
+++ js/storage.js
@@ -1,12 +1,17 @@
+/** These lines define the token and url for the remote storage. */
 const STORAGE_TOKEN = 'K9SFYBX0R1WD1NS9ZKIOB4W1CL157WB4C579A62K';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
-4
+
+
+/** This function is used to save items in the remote storage. */
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
         .then(res => res.json());
 }
 
+
+/** This function is used to load items from the remote storage */
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
     return fetch(url).then(res => res.json()).then(res => {
@@ -15,6 +20,3 @@ async function getItem(key) {
         } throw `Could not find data with key "${key}".`;
     });
 }
-
-
-
