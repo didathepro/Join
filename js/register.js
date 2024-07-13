90ae6899645beeef945befce9bfc5e4f2a8d6709
Andreas W. <bahamut13+github0@gmail.com>
2024-07-14 01:33:08 +0200

    Correction of wrong spelled register.js, JSDoc, max 14 lines and cleanup

--- js/register.js
+++ js/register.js
@@ -1,61 +1,44 @@
+/** These lines define the elements for password, name and email. */
 let passwordReg = document.getElementById('passwordInput');
 let passwordConfirmReg = document.getElementById('passwordConfirmInput');
 let namesReg = document.getElementById('namesInput');
 let emailReg = document.getElementById('emailInput');
 
 
-/**
- * Inizialiesiert Body Onload and load UserData
- */
+/** This function loads the users on initilization. */
 async function initReg() {
     await loadUsers();
 }
 
-/**
- * load Usere from LocalStorageServer
- */
 
-/**
- * checked first password and passwordConfirm are the Same
- * @returns if false then alert will show Up to Message you
- * when all Correct, Data will be Saved on LocalStorage to a JSON;
- */
+/** This function checks if both passwords are identical and then saves the information. */
 async function register() {
-    const password = passwordReg.value;
-    const passwordConfirm = passwordConfirmReg.value;
-    const name = namesReg.value;
-    const email = emailReg.value;
-
-    if (password !== passwordConfirm) {
+    if (passwordReg.value !== passwordConfirmReg.value) {
         alert('Passwords are not the same');
         return;
     }
-
     signBtn.disabled = true;
     users.push({
-        name: name,
-        email: email,
-        password: password,
+        name: namesReg.value,
+        email: emailReg.value,
+        password: passwordReg.value,
     });
     await setItem('users', JSON.stringify(users));
-
     msg();
     resetForm();
     backToLogin();
 }
 
-/**
- * if the Regestration was Successfull then a message will show UP
- */
+
+/** This function generates a message if the signup was successful. */
 function msg() {
     document.getElementById('msgBox').innerHTML = 'You Signed Up successfully';
     document.getElementById('msgBox').style.display = 'flex';
 
 }
 
-/**
- * this function will bring you back to Login site after successful regestration
- */
+
+/** This funcion redirects to the login page after successful signup. */
 function backToLogin() {
     setTimeout(function () {
         window.location.href = "logIn.html";
@@ -63,9 +46,8 @@ function backToLogin() {
 
 }
 
-/**
- * after submit succesfull the Form, all Inputs will be rested
- */
+
+/** This function resets the input fields after the form has been submitted. */
 function resetForm() {
     namesReg.value = ''
     emailReg.value = '';
@@ -74,16 +56,13 @@ function resetForm() {
     signBtn.disabledReg = false;
 }
 
-/**
- * this function show up and visible your password while you type onclick on the Image
- */
+
+/** This function toggles the visibility of the password. */
 function showPassword() {
     let password = document.getElementById('passwordInput');
-
     if (password.type === 'password') {
         password.type = 'text';
         document.getElementById('passwordImg').src = "./assets/img/visibility_off.png"
-
     }
     else {
         password.type = 'password';
@@ -91,12 +70,10 @@ function showPassword() {
     }
 }
 
-/**
- * this function is Similar to showPassword() function but only for the ConfirmPassword
- */
+
+/** This function toggles the visibility of the confirm password. */
 function showPasswordConfirm() {
     let passwordConfirm = document.getElementById('passwordConfirmInput');
-
     if (passwordConfirm.type === 'password') {
         passwordConfirm.type = 'text';
         document.getElementById('passwordImgConfirm').src = "./assets/img/visibility_off.png"
