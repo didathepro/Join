ff7dbea6eecd398eff869804554b584d5052bd70
didathepro <didovicgraz@gmail.com>
2024-07-21 16:20:15 +0200

    fixed register

--- js/register.js
+++ js/register.js
@@ -1,15 +1,15 @@
 /** These lines define the elements for password, name and email. */
-let passwordReg = document.getElementById('passwordInput');
-let passwordConfirmReg = document.getElementById('passwordConfirmInput');
-let namesReg = document.getElementById('namesInput');
-let emailReg = document.getElementById('emailInput');
+let passwordReg, passwordConfirmReg, namesReg, emailReg, signBtn;
 
-
-/** This function loads the users on initilization. */
+/** This function loads the users on initialization. */
 async function initReg() {
     await loadUsers();
+    passwordReg = document.getElementById('passwordInput');
+    passwordConfirmReg = document.getElementById('passwordConfirmInput');
+    namesReg = document.getElementById('namesInput');
+    emailReg = document.getElementById('emailInput');
+    signBtn = document.getElementById('signBtn');
 }
-
 
 /** This function checks if both passwords are identical and then saves the information. */
 async function register() {
@@ -29,56 +29,47 @@ async function register() {
     backToLogin();
 }
 
-
 /** This function generates a message if the signup was successful. */
 function msg() {
     document.getElementById('msgBox').innerHTML = 'You Signed Up successfully';
     document.getElementById('msgBox').style.display = 'flex';
-
 }
 
-
-/** This funcion redirects to the login page after successful signup. */
+/** This function redirects to the login page after successful signup. */
 function backToLogin() {
     setTimeout(function () {
         window.location.href = "logIn.html";
     }, 3000);
-
 }
-
 
 /** This function resets the input fields after the form has been submitted. */
 function resetForm() {
-    namesReg.value = ''
+    namesReg.value = '';
     emailReg.value = '';
     passwordReg.value = '';
     passwordConfirmReg.value = '';
-    signBtn.disabledReg = false;
+    signBtn.disabled = false;
 }
-
 
 /** This function toggles the visibility of the password. */
 function showPassword() {
     let password = document.getElementById('passwordInput');
     if (password.type === 'password') {
         password.type = 'text';
-        document.getElementById('passwordImg').src = "./assets/img/visibility_off.png"
-    }
-    else {
+        document.getElementById('passwordImg').src = "./assets/img/visibility_off.png";
+    } else {
         password.type = 'password';
-        document.getElementById('passwordImg').src = "./assets/img/lock.png"
+        document.getElementById('passwordImg').src = "./assets/img/lock.png";
     }
 }
-
 
 /** This function toggles the visibility of the confirm password. */
 function showPasswordConfirm() {
     let passwordConfirm = document.getElementById('passwordConfirmInput');
     if (passwordConfirm.type === 'password') {
         passwordConfirm.type = 'text';
-        document.getElementById('passwordImgConfirm').src = "./assets/img/visibility_off.png"
-    }
-    else {
+        document.getElementById('passwordImgConfirm').src = "./assets/img/visibility_off.png";
+    } else {
         passwordConfirm.type = 'password';
         document.getElementById('passwordImgConfirm').src = "./assets/img/lock.png";
     }
